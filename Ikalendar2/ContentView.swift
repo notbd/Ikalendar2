39ffea6643781b6f4e0ae41f7822f8c23cacda83
Tianwei Zhang <zhang13music@outlook.com>
2022-08-19 17:41:55 -0400

    Initial Commit

---
+++ Ikalendar2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Ikalendar2
+//
+//  Created by Tianwei Zhang on 8/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ikalendar2/Ikalendar2App.swift
@@ -0,0 +1,17 @@
+//
+//  Ikalendar2App.swift
+//  Ikalendar2
+//
+//  Created by Tianwei Zhang on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Ikalendar2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
